f7a3e92621a732f73daf623af0f20c31b7133f94
coolcodeer <105897964+coolcodeer@users.noreply.github.com>
2023-10-09 21:19:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX=0
+noseY=0
+
+function preload(){
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+}
+function setup(){
+    canvas=createCanvas(300,300)
+    canvas.center()
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video, modelloaded)
+    poseNet.on("pose", gotposes);
+
+    
+}
+function draw(){
+    image(video, 0, 0, 300, 300)
+    image(mustache, noseX - 20, noseY + 7, 50,20)
+}
+function take_snapshot(){
+    save("p.png")
+}
+
+function modelloaded(){
+    console.log("poseNet is loaded");
+
+}
+function gotposes(results){
+    if (results.length > 0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+    }
+}
